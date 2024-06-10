62ac311c708a821a038d7fe6a80135b394286d45
edrikfsteiner <edrikfsteiner@gmail.com>
2024-06-10 16:03:12 -0300

    Update Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,20 +3,25 @@ import { FaUser, FaLock } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
-const Login = () => {
+function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [selectedRole, setSelectedRole] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
     console.log("Dados de Login:", { username, password });
+  };
+
+  const selectButton = (role) => {
+    setSelectedRole(role);
   };
 
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
         <h1>Login</h1>
+
         <div className="input-field">
           <input
             type="text"
@@ -27,6 +32,7 @@ const Login = () => {
           />
           <FaUser className="icon" />
         </div>
+
         <div className="input-field">
           <input
             type="password"
@@ -37,6 +43,19 @@ const Login = () => {
           />
           <FaLock className="icon" />
         </div>
+
+        <div className="option-container">
+          <div className="option-button" onClick={() => selectButton('vendedor')}>
+            <div className={`circle ${selectedRole === 'vendedor' && 'selected'}`} id="vendedor"></div>
+            <span>&nbsp;Vendedor</span>
+          </div>
+          
+          <div className="option-button" onClick={() => selectButton('gerente')}>
+            <div className={`circle ${selectedRole === 'gerente' && 'selected'}`} id="gerente"></div>
+            <span>&nbsp;Gerente</span>
+          </div>
+        </div>
+        
         <Link to={"/dashboard"}><button type="submit">Login</button></Link>
       </form>
     </div>
