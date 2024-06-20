1b9b1dfcc2f3ae88f016c0232c7a72e75ef8f228
edrikfsteiner <edrikfsteiner@gmail.com>
2024-06-20 14:56:53 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './Components/Login/Login'
-import Vendedor from './Components/Vendedor/Vendedor'
-import Gerente from './Components/Gerente/Gerente'
+import Salesman from './Components/Salesman/Salesman'
+import Manager from './Components/Manager/Manager'
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login/>}/>
-        <Route path='/vendedor' element={<Vendedor/>}/>
-        <Route path='/gerente' element={<Gerente/>}/>
+        <Route path='/salesman' element={<Salesman/>}/>
+        <Route path='/manager' element={<Manager/>}/>
       </Routes>
     </BrowserRouter>
   )
