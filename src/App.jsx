d85a11713b9b562fb4527297617543489bcf23f6
edrikfsteiner <edrikfsteiner@gmail.com>
2024-06-20 19:38:37 -0300

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './Components/Login/Login'
-import Salesman from './Components/Salesman/Salesman'
-import Manager from './Components/Manager/Manager'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './Components/Login/Login';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Salesman from './Components/Salesman/Salesman';
+import Manager from './Components/Manager/Manager';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/salesman' element={<Salesman/>}/>
         <Route path='/manager' element={<Manager/>}/>
       </Routes>
@@ -16,4 +18,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
