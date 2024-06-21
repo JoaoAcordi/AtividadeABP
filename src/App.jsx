cc1ce08e148e182264fb44adb886967a665ccd0c
edrikfsteiner <edrikfsteiner@gmail.com>
2024-06-21 19:19:53 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Login from './Components/Login/Login';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Salesman from './Components/Salesman/Salesman';
 import Manager from './Components/Manager/Manager';
+import { SalesProvider } from './Components/Salesman/SalesContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/salesman' element={<Salesman/>}/>
-        <Route path='/manager' element={<Manager/>}/>
-      </Routes>
-    </BrowserRouter>
+    <SalesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/salesman' element={<Salesman/>}/>
+          <Route path='/manager' element={<Manager/>}/>
+        </Routes>
+      </BrowserRouter>
+    </SalesProvider>
   )
 }
 
