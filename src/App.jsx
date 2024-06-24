26f7acb057742c9c0a8473076d4820b611356c44
edrikfsteiner <edrikfsteiner@gmail.com>
2024-06-24 16:42:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login/Login';
+import { AuthProvider } from './Components/Login/AuthContext';
 import Dashboard from './Components/Dashboard/Dashboard';
-import Salesman from './Components/Salesman/Salesman';
-import Manager from './Components/Manager/Manager';
-import { SalesProvider } from './Components/Salesman/SalesContext';
+import Sales from './Components/Sales/Sales';
+import { SalesProvider } from './Components/Sales/SalesContext';
+import Stock from './Components/Stock/Stock';
+import { StockProvider } from './Components/Stock/StockContext';
 
 function App() {
   return (
-    <SalesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/salesman' element={<Salesman/>}/>
-          <Route path='/manager' element={<Manager/>}/>
-        </Routes>
-      </BrowserRouter>
-    </SalesProvider>
-  )
+    <AuthProvider>
+      <SalesProvider>
+        <StockProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Login/>}/>
+              <Route path='/dashboard' element={<Dashboard/>}/>
+              <Route path='/sales' element={<Sales/>}/>
+              <Route path='/stock' element={<Stock/>}/>
+            </Routes>
+          </BrowserRouter>
+        </StockProvider>
+      </SalesProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
