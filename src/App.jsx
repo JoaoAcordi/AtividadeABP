19adfe6a7d824c31f8ea27e9da250664f3e7c535
edrikfsteiner <edrikfsteiner@gmail.com>
2024-06-27 22:09:59 -0300

    Update src

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Login from './Components/Login/Login';
 import { AuthProvider } from './Components/Login/AuthContext';
 import Dashboard from './Components/Dashboard/Dashboard';
@@ -7,6 +6,7 @@ import Sales from './Components/Sales/Sales';
 import { SalesProvider } from './Components/Sales/SalesContext';
 import Stock from './Components/Stock/Stock';
 import { StockProvider } from './Components/Stock/StockContext';
+import './App.css';
 
 function App() {
   return (
@@ -15,10 +15,10 @@ function App() {
         <StockProvider>
           <BrowserRouter>
             <Routes>
-              <Route path='/' element={<Login/>}/>
-              <Route path='/dashboard' element={<Dashboard/>}/>
-              <Route path='/sales' element={<Sales/>}/>
-              <Route path='/stock' element={<Stock/>}/>
+              <Route path='/' element={<Login />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+              <Route path='/sales' element={<Sales />} />
+              <Route path='/stock' element={<Stock />} />
             </Routes>
           </BrowserRouter>
         </StockProvider>
